fe30f63329cfb37c0d69ca97025c44e88ad0e298
Shishir-Kc <shishir.khatri.kc@gmail.com>
2026-08-16 19:47:05 +0545

    chor:testing things out

--- RusttyObject/src/main.rs
+++ RusttyObject/src/main.rs
@@ -2,5 +2,8 @@ mod brand;
 mod cli;
 fn main() {
     brand::brand();
-    println!("{}",cli::get_user_github_repo_url());
+    // println!("{}",cli::get_user_github_repo_url());
+    println!("Home Dir => {}", std::env::var("HOME").unwrap());
+    cli::show_options();
+    cli::run_cli();
 }
